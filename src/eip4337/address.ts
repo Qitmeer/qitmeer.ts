850de5a8d71b7e3470be96ddf09954f2631b31a9
Jin <lochjin@gmail.com>
2024-10-12 23:26:13 +0800

    feat:meerchange address

--- src/eip4337/address.ts
+++ src/eip4337/address.ts
@@ -1,4 +1,4 @@
 export const EntryPointAddr = `0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789`;
 export const QngAccountFactoryAddr = `0xa95a633294a8ce18298B488858d90c4E16FD0AB0`;
 export const QngPaymasterAddr = `0x016001559A8685af58C45f0d6408fF88075343c5`;
-export const MeerChangeAddr = `0x7a9a241E8AD3D9804d4545a86DeDb93b397C9899`;
+export const MeerChangeAddr = `0x7D698C4E800dBc1E9B7e915BefeDdB59Aa9E8BB6`;
