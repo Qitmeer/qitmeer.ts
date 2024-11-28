4fddfc45c67d013d92d32794460e1d8eb7550891
frankcrypto2023 <cp6svaen@hotmail.com>
2024-11-28 13:39:16 +0800

    dependence contract address

--- src/eip4337/address.ts
+++
@@ -1,4 +0,0 @@
-export const EntryPointAddr = `0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789`;
-export const QngAccountFactoryAddr = `0xb979a65B7168FC48284D9afF1A079436761A21EB`;
-export const QngPaymasterAddr = `0xa9E0107cE8340D7E025885be51ce53F467dCebC1`;
-export const MeerChangeAddr = `0x7D698C4E800dBc1E9B7e915BefeDdB59Aa9E8BB6`;
